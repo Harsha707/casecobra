ae0920680e6e0dc2ff6186ca54ce5d40bd4fca58
Harsha vardhan <harshayanamadala7@gmail.com>
2024-10-05 22:50:40 +0530

    test

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -5,52 +5,52 @@ import { validateWebhookSignature } from "razorpay/dist/utils/razorpay-utils";
 
 export async function POST(req: Request) {
   try {
-    const webhookBody = await req.text(); // Get raw request body for signature verification
-    const webhookSignature = headers().get("x-razorpay-signature");
+    // const webhookBody = await req.text(); // Get raw request body for signature verification
+    // const webhookSignature = headers().get("x-razorpay-signature");
 
-    if (!webhookSignature) {
-      return new Response("Invalid signature", { status: 400 });
-    }
+    // if (!webhookSignature) {
+    //   return new Response("Invalid signature", { status: 400 });
+    // }
 
-    const webhookSecret = process.env.RAZORPAY_WEBHOOK_SECRET!;
+    // const webhookSecret = process.env.RAZORPAY_WEBHOOK_SECRET!;
 
-    // Validate the webhook signature
-    const isValidSignature = validateWebhookSignature(
-      webhookBody,
-      webhookSignature,
-      webhookSecret
-    );
+    // // Validate the webhook signature
+    // const isValidSignature = validateWebhookSignature(
+    //   webhookBody,
+    //   webhookSignature,
+    //   webhookSecret
+    // );
 
-    if (!isValidSignature) {
-      return new Response("Signature mismatch", { status: 400 });
-    }
+    // if (!isValidSignature) {
+    //   return new Response("Signature mismatch", { status: 400 });
+    // }
 
-    // Parse the JSON body after validation
-    const event = JSON.parse(webhookBody);
+    // // Parse the JSON body after validation
+    // const event = JSON.parse(webhookBody);
 
-    if (event.event === "payment.captured") {
-      const payment = event.payload.payment.entity;
+    // if (event.event === "payment.captured") {
+    //   const payment = event.payload.payment.entity;
 
-      const { userId, orderId } = payment.notes || {
-        userId: null,
-        orderId: null,
-      };
+    //   const { userId, orderId } = payment.notes || {
+    //     userId: null,
+    //     orderId: null,
+    //   };
 
-      if (!userId || !orderId) {
-        throw new Error("Invalid request metadata");
-      }
+    //   if (!userId || !orderId) {
+    //     throw new Error("Invalid request metadata");
+    //   }
 
-      // Update the order in the database
-      await db.order.update({
-        where: { id: orderId },
-        data: {
-          isPaid: true,
-          paymentId: payment.id,
-        },
-      });
-    }
+    //   // Update the order in the database
+    //   await db.order.update({
+    //     where: { id: orderId },
+    //     data: {
+    //       isPaid: true,
+    //       paymentId: payment.id,
+    //     },
+    //   });
+    // }
 
-    return NextResponse.json({ result: event, ok: true });
+    return NextResponse.json({ ok: true });
   } catch (err) {
     console.error(err);
     return NextResponse.json(
