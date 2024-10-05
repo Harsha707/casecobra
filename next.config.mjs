0072dcd464e9b2fbada71be5f6d5edb0edf39577
Harsha vardhan <harshayanamadala7@gmail.com>
2024-10-05 13:40:44 +0530

    Solve cors error

--- next.config.mjs
+++ next.config.mjs
@@ -6,19 +6,16 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
+        // Apply headers to all routes
+        source: "/:path*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "*" }, // Allow all origins
           {
             key: "Access-Control-Allow-Methods",
             value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-          },
-          {
-            key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
-          },
+          }, // Allow all methods
+          { key: "Access-Control-Allow-Headers", value: "*" }, // Allow all headers
         ],
       },
     ];
